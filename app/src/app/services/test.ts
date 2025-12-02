5f385b668126089c270c29698267a669b97d506f
filhed97 <filhed97@gmail.com>
2025-12-02 19:16:18 +0100

    fix(frontend): Update test.ts to use correct environment path to endpoint

--- app/src/app/services/test.ts
+++ app/src/app/services/test.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Test {
-  private apiUrl = 'http://localhost:7071/api/TestFunction'; // Function URL
+  private apiUrl = `${environment.apiBaseUrl}/AuthRegister`;
 
   constructor(private http: HttpClient) { }
 
