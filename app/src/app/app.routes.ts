620bb21c982143c8997ca1a25989929443425e71
filhed97 <filhed97@gmail.com>
2025-12-03 19:15:17 +0100

    feat(app): update routes to include BooksPage

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { BooksPage } from './pages/books/books';
 
 export const routes: Routes = [
+  { path: '', component: BooksPage },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
 ];
